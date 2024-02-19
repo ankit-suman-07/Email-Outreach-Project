0e2fecad2a149a116cbf15c4ccc4ea6137d4952e
ankit-suman-07 <ankitsuman07@gmail.com>
2024-02-19 21:29:29 +0530

    Case Study - in-app chat

---
+++ demo.js
@@ -0,0 +1,70 @@
+// import React, { useState } from 'react'
+
+// const demo = () => {
+//     const [notes, setNotes] = useState("");
+//     const [title, setTitle] = useState("");
+
+//     const fetch = () => {
+//         const notesData = axios.get('abc.com/notes')
+//     }
+
+//     useEffect(() => {
+
+//     }, [])
+
+
+//     return (
+//         <div>
+//             <form>
+
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default demo
+
+
+// app.get('/', function (), (req, res, next)) {
+
+// }
+
+// app.listen(5500, () => {
+//     console.log('Server')
+// })
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i, ' JavaScript')
+//     }
+//     else if (i % 3 === 0) {
+//         console.log(i, ' Java')
+//     }
+//     else if (i % 5 === 0) {
+//         console.log(i, ' Script')
+//     }
+// }
+
+
+// class Animal {
+//     static belly = [];
+//     eat() {
+//         Animal.belly.push('food');
+//     }
+// }
+// let a = new Animal();
+// a.eat();
+// console.log(Animal.belly[0]);
+
+// class Node {
+//     Node() {
+        
+//     }
+// }
+
+// int count = 0;
+// Node current = Node.head;
+// while (current.next != null) {
+//     count += 1;
+//     current = current.next;
+// }
